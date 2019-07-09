9ce0ed10c2e5fcb970b6dc81d3e31265ca7cc038
Juan Polanco <juanmpt@outlook.com>
2019-07-09 17:32:01 +0200

    Added the article controller and some views

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+fake_articles = Article.create({title: Faker::Book.title, content: Faker::Quote.famous_last_words})
+fake_articles = Article.create({title: Faker::Book.title, content: Faker::Quote.famous_last_words})
+fake_articles = Article.create({title: Faker::Book.title, content: Faker::Quote.famous_last_words})
+fake_articles = Article.create({title: Faker::Book.title, content: Faker::Quote.famous_last_words})
+fake_articles = Article.create({title: Faker::Book.title, content: Faker::Quote.famous_last_words})
+fake_articles = Article.create({title: Faker::Book.title, content: Faker::Quote.famous_last_words})
+fake_articles = Article.create({title: Faker::Book.title, content: Faker::Quote.famous_last_words})
+fake_articles = Article.create({title: Faker::Book.title, content: Faker::Quote.famous_last_words})
+fake_articles = Article.create({title: Faker::Book.title, content: Faker::Quote.famous_last_words})
+fake_articles = Article.create({title: Faker::Book.title, content: Faker::Quote.famous_last_words})
+fake_articles = Article.create({title: Faker::Book.title, content: Faker::Quote.famous_last_words})
+
